3a27ab17229c646762dcfbfd86918f605f772576
ChaosCrider <ChaosCrider@hotmail.com>
2024-06-09 21:18:46 -0400

    converted to War packages

--- src/main/java/bdeb/programmation/TP_A18/TpA18Application.java
+++ src/main/java/bdeb/programmation/TP_A18/TpA18Application.java
@@ -2,12 +2,15 @@ package bdeb.programmation.TP_A18;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TpA18Application {
+public class TpA18Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TpA18Application.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TpA18Application.class);
 	}
 
 }
